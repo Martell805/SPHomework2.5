649005e9403aad2c9896e95bde960897e4cc5837
Martell <vovasamb@yandex.ru>
2022-07-24 12:52:35 +0300

    Initial commit

---
+++ src/test/java/pro/sky/employeesaver/EmployeeSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeesaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

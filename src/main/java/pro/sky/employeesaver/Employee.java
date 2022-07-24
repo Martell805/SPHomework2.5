ce8d2d1dbd3c95f497c86ce0a2b2d469abc5cdb5
Martell <vovasamb@yandex.ru>
2022-07-24 20:18:48 +0300

    @Data вместо геттеров и сеттеров

--- src/main/java/pro/sky/employeesaver/Employee.java
+++ src/main/java/pro/sky/employeesaver/Employee.java
@@ -1,7 +1,8 @@
 package pro.sky.employeesaver;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Employee {
     private String name;
     private String surname;
@@ -9,42 +10,5 @@ public class Employee {
     public Employee(String name, String surname) {
         this.name = name;
         this.surname = surname;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, surname);
     }
 }
